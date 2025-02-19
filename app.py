99fe43f4cf9c72614a7eece9ff6c92dca785c792
YogeshAtigre <yogeshatigre2000@gmail.com>
2025-02-20 00:09:55 +0530

    Update app.py
    ip update

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return 'Hello, Jenkins CI/CD Pipeline with Flask Application!'
 
 if __name__ == '__main__':
-    app.run(host='34.238.160.181', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
